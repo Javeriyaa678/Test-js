f82206af5089ce3f88bc458f3f96d4811f384fd6
Javeriyaa678 <javeriyan678@gmail.com>
2024-05-13 12:01:13 -0700

    create 01-varibles

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,15 @@
+const account_Id = 144553
+let AccountEmail = "info@google.com"
+var AccPass = "123456"
+
+accountCity = "Karachi"
+
+// account_Id  = 2
+
+
+
+AccountEmail = "hhhs@hjks.com"
+AccPass = "56874"
+accountCity = "islamabad"
+
+console.table([account_Id, accountCity, AccPass, AccountEmail])
